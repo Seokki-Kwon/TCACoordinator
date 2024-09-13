8701c8dd58771309c09be3ca8bbcbc5569c98268
Seokki-Kwon <seokki0706@gmail.com>
2024-09-13 17:15:56 +0900

    Initial Commit

---
+++ TCACoordinator/TCACoordinatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCACoordinatorApp.swift
+//  TCACoordinator
+//
+//  Created by 권석기 on 9/13/24.
+//
+
+import SwiftUI
+
+@main
+struct TCACoordinatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
